c9a15e0d2c87b7b6b6dc41f550b9976e934cccc5
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-22 17:32:58 +0530

    Add files via upload

---
+++ WPT/Node.js/file1.js
@@ -0,0 +1,9 @@
+let num1=100, num2=200;
+num1+num2
+let fname="Pawan", lname="Lekurwale";
+fname+lname;
+
+function helloNode(name){
+return " Welome sir"+name+"  in infoway";
+}
+helloNode("Node.js");

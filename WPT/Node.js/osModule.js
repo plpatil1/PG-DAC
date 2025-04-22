ba7c0f7c9fd911885f90f1ff5ee46560e241b2d5
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-22 17:29:05 +0530

    Add files via upload

---
+++ WPT/Node.js/osModule.js
@@ -0,0 +1,15 @@
+const os = require('os');
+
+console.log("This is Operating System Module ");
+
+console.log(os.hostname());
+console.log(os.homedir());
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.type());
+console.log(os.cpus());
+console.log(os.availableParallelism());
+console.log(os.version());
+console.log(os.userInfo());
+console.log(os.tmpdir());

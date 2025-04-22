5c73fe61805fa63c44890c22422b80bbfa74343e
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-22 17:29:41 +0530

    Add files via upload

---
+++ WPT/Node.js/module.js
@@ -0,0 +1,9 @@
+// const addition = require('./arithmatic');
+const arithmatic = require('./arithmatic');
+// const multiplication = require('./arithmatic');
+// const division = require('./arithmatic');
+
+console.log(arithmatic.addition(500, 600));
+console.log(arithmatic.substraction(500, 600));
+console.log(arithmatic.multiplication(500, 600));
+console.log(arithmatic.division(500, 6));
